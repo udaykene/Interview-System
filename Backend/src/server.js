a32c6d943ddcc5248304263ca0f63b3517ac752d
udaykene <udaykene96@gmail.com>
2026-01-24 14:46:22 +0530

    Chnaged the server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -15,10 +15,10 @@ app.get("/health", (req, res) => {
 
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.use(express.static(path.join(__dirname, "Frontend/dist")));
 
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
   });
 }
 
