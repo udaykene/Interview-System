b67513ed204577bcf7b5930b756bde7da9ccb94e
udaykene <udaykene96@gmail.com>
2026-01-24 13:51:14 +0530

    Add deployment features

--- Backend/src/lib/env.js
+++ Backend/src/lib/env.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const ENV = {
-    PORT:process.env.port,
+    PORT:process.env.PORT,
     DB_URL:process.env.DB_URL,
+    NODE_ENV:process.env.NODE_ENV
 }
