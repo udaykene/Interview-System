d03ee69599767baee36f1a2dff8c9ad57482524b
udaykene <udaykene96@gmail.com>
2026-01-25 15:23:13 +0530

    feat: integrate Clerk authentication with SignIn and SignOut functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,27 @@
+import { SignInButton, SignOutButton, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
 import React from 'react'
 
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <h1>Welcome to Interview Platform</h1>
+      
+      {/* This only shows if the user is LOGGED OUT */}
+      <SignedOut>
+        <SignInButton mode="modal">
+          <button className="your-style">Sign In</button>
+        </SignInButton>
+      </SignedOut>
+
+      {/* This only shows if the user is LOGGED IN */}
+      <SignedIn>
+        <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+          <p>You are logged in!</p>
+          <UserButton />
+          {/* Or a simple SignOutButton */}
+          <SignOutButton />
+        </div>
+      </SignedIn>
     </div>
   )
 }
