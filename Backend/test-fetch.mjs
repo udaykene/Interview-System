c1127fcfb6fd7eb56f14f2ac7b5e14f0571dc990
udaykene <udaykene96@gmail.com>
2026-05-08 14:13:40 +0530

    refactor: remove redundant and unused utility files from the codebase

--- Backend/test-fetch.mjs
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-fetch('https://raw.githubusercontent.com/engineer-man/piston/master/packages/java/15.0.2/run')
-  .then(res => res.text())
-  .then(text => console.log(text));
--- Backend/test-judge.js
+++
@@ -1,4 +0,0 @@
-import { buildTestRunner } from './src/controllers/executeController.js';
-
-// Wait, executeController.js exports runCode, submitCode, but buildTestRunner is NOT exported.
-// Let's copy the needed functions from executeController.js to test.
